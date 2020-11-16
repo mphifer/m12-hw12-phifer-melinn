9be8e74f9e67ba8cb0460383f78da05fd78c9f33
Melinn Phifer <mphifer@ufl.edu>
2020-11-16 09:01:18 -0500

    add js to main.js

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # m12-hw12-phifer-melinn
+https://www.w3schools.com/js/js_array_sort.asp
+
+https://www.w3schools.com/js/js_arrow_function.asp
+
+https://www.w3schools.com/js/tryit.asp?filename=tryjs_arrow_function7
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+
+https://forum.freecodecamp.org/t/click-button-issue-works-only-once-then-i-need-to-refresh-the-page-for-it-work-again-solved-now-stuck-with-social-share-buttons/178580/2
--- src/main.js
+++ src/main.js
@@ -1,1 +1,40 @@
 console.log('test');
+
+const parks = [
+"Arcadia National Park",
+"Big Bend National Park",
+"Black Canyon National Park",
+"Crater Lake National Park",
+"Grand Canyon National Park",
+"Kenai Fjords National Park",
+"Lake Clark National Park",
+"Olympic National Park",
+"Redwood National Park",
+"White Sands National Park" ];
+//
+// let rand_parks = Math.floor(Math.random() * 10);
+//
+// let parkRandom = parks[rand_parks];
+//
+// // const getNewItem = document.getElementById('click-me');
+// // getNewItem.addEventListener('click', myFunction);
+//
+// document.getElementById("click-me").addEventListener("click",
+//   function myFunction() {
+//     document.getElementById("place").innerHTML = parkRandom;
+//   }, false
+// );
+let randomParks;
+
+randomParks = () => {
+let i, j, k;
+for (i = parks.length -1; i > 0; i--) {
+  j = Math.floor(Math.random() * i)
+  k = parks[i]
+  parks[i] = parks[j]
+  parks[j] = k
+}
+document.getElementById("place").innerHTML = parks[0];
+};
+
+document.getElementById("click-me").addEventListener("click", randomParks);
